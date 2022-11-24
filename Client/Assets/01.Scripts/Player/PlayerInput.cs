8de1a5bc9f4b6fe444d45f355dc94256293d37eb
SEH00N <06psh0501@gmail.com>
2022-11-24 20:26:24 +0900

    oxygen control & some prefab

--- Client/Assets/01.Scripts/Player/PlayerInput.cs
+++ Client/Assets/01.Scripts/Player/PlayerInput.cs
@@ -7,20 +7,35 @@ using static Define;
 
 public class PlayerInput : MonoBehaviour
 {
+    [SerializeField] int maxJumpCount = 3;
+    private int _currentJumpCount = 0;
+
+    [Space(10f)]
     public UnityEvent<Vector3> OnJumpInput;
 
-    void Update()
+    private void Update()
     {
         GetJumpInput();
     }
 
     private void GetJumpInput()
     {
-        if(Input.GetMouseButtonDown(0)) {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            if(_currentJumpCount >= maxJumpCount)
+                return;
+
+            _currentJumpCount++;
             Vector3 mousePos = MainCam.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
             Debug.Log("Jump Input");
             OnJumpInput?.Invoke(mousePos);
         }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+            _currentJumpCount = 0;
     }
 }
