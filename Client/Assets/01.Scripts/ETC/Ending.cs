7d85b0db456fc01b00eb15d7203053b2765c0c36
hueam <72968569+hueam@users.noreply.github.com>
2023-01-06 19:36:37 +0900

    fix

--- Client/Assets/01.Scripts/ETC/Ending.cs
+++ Client/Assets/01.Scripts/ETC/Ending.cs
@@ -14,20 +14,25 @@ public class Ending : MonoBehaviour
     [SerializeField] Button Restart;
     [SerializeField] Button GotoMenu;
     [SerializeField] TextMeshProUGUI text;
-    [SerializeField] AudioSource audioSource;
-
+    [SerializeField] AudioSource BGM;
+    [SerializeField] AudioSource fishSource;
+    public void Doma(){
+        AudioManager.Instance.PlayAudio("찰박",fishSource);
+    }
     public void StartEnding(){
         StartCoroutine(ShowEnding());
     }
     IEnumerator ShowEnding(){
         image.color = Color.black;
-        AudioManager.Instance.PlayAudio("칼소리",audioSource);
-        yield return new WaitForSeconds(2.5f);
-        AudioManager.Instance.PlayAudio("보글보글",audioSource);
         firstCm.Priority = 0;
         secondCm.Priority = 10;
+        AudioManager.Instance.PlayAudio("칼소리",fishSource);
+        yield return new WaitForSeconds(2.5f);
+        AudioManager.Instance.PlayAudio("보글보글",fishSource);
+        AudioManager.Instance.PlayAudio("EndingBGM",BGM);
+        yield return new WaitForSeconds(1f);
         Sequence seq = DOTween.Sequence();
-        seq.Append(image.DOFade(0,1f));
+        seq.Append(image.DOFade(0,4f));
         seq.Join(secondCm.transform.DOMove(secondCm.transform.position+ secondCm.transform.forward * -1.4f,4f));
         yield return new WaitForSeconds(4f);
         text.DOFade(1,1f);
@@ -38,6 +43,7 @@ public class Ending : MonoBehaviour
         seq.Kill();
     }
     public void BtnEvet(string name){
+
         SceneLoader.Instance.LoadAsync(name);
     }
 }
